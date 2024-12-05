86c81689a013edfe46e6fc0c489e32c5d70ab48a
Emnaothm96 <emna.othmani@ensea.fr>
2024-12-05 08:48:11 +0100

    Update question 1

--- enseash.h
+++ enseash.h
@@ -1,1 +1,12 @@
-void print_welcome_message();
+#include <stdio.h>
+#include <unistd.h>
+#ifndef ENSEASH_H
+#define ENSEASH_H
+
+#define BUFFER_SIZE 128
+
+void print_message(const char *message);
+
+ssize_t read_input(char *buffer, size_t size);
+
+#endif // ENSEASH_H
