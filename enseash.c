e3ddd0702f3353d39624bd2c6fbb0322c7da4490
Emnaothm96 <emna.othmani@ensea.fr>
2024-12-05 08:31:25 +0100

    question 1

---
+++ enseash.c
@@ -0,0 +1,9 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "function.h"
+#include "enseash.h"
+
+int main(){
+    print_welcome_message();        
+}
---
+++ function.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "function.h"
+#define MAX_INPUT_SIZE 1024
+char *welcome_message = "Bienvenue dans le Shell ENSEA.\nPour quitter, tapez \'exit\'\n";
+char *prompt_message = "enseash % ";
+
+
+void print_welcome_message(){
+    char input[MAX_INPUT_SIZE];
+    write(STDOUT_FILENO, welcome_message, strlen(welcome_message));
+    write(STDOUT_FILENO, prompt_message, strlen(prompt_message));
+    read(STDIN_FILENO,input,strlen(input));
+}
